25fee2442f2b82bc3ac2ed56ee821f8386616aa6
Carlos Chen <128018697+CarlosChenCid@users.noreply.github.com>
2023-05-16 14:39:40 -0500

    Covid.sql

--- covid.sql
+++ covid.sql
@@ -57,3 +57,53 @@ JOIN covid.vaccinations v
     and d.date = v.date
 WHERE d.continent is not null
 order by 1,2,3;
+
+-- Table to export to Tableau (1)
+Select location, SUM(cast(new_deaths as real)) as TotalDeathCount
+From covid.deaths
+Where continent is not null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc;
+
+-- 2 PercentPopulationInfected
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From covid.deaths
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+-- 3 PercentPopulationInfected with date
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From covid.deaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc;
+
+-- 4 RollingPeopleVaccinated
+Select dea.continent, dea.location, dea.date, dea.population
+, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
+From Covid.Deaths dea
+Join covid.vaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+group by dea.continent, dea.location, dea.date, dea.population
+order by 1,2,3;
+
+-- 5 total_deaths with date
+Select Location, date, population, total_cases, total_deaths
+From Covid.Deaths
+where continent is not null 
+order by 1,2;
+
+-- 6 Join table
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations as real)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+From Covid.Deaths dea
+Join Covid.Vaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+)
